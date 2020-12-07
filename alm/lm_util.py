fix NLL computation

alm/lm_util.py
@@ -231,9 +231,9 @@ class TransformersLM:
         assert type(texts) is list, 'type error'
         assert not self.embedding_mode
         data_loader, partition = self.batch_encode_plus_mask(texts, batch_size=batch_size)
-        loglikelihood = self.__get_ll(data_loader)
+        nll = self.__get_nll(data_loader)
         # for pseudo likelihood aggregation
-        return list(map(lambda x: math.exp(- sum(loglikelihood[x[0]:x[1]]) / (x[1] - x[0])), partition))
+        return list(map(lambda x: math.exp(sum(nll[x[0]:x[1]]) / (x[1] - x[0])), partition))
 
     def get_nll(self, texts: List, tokens_to_mask: List, batch_size: int = None):
         """ to compute negative log likelihood of a masked token
@@ -246,13 +246,12 @@ class TransformersLM:
         assert type(texts) is list and type(tokens_to_mask) is list, 'type error'
         assert not self.embedding_mode or self.is_causal
         data_loader, _ = self.batch_encode_plus_mask(texts, batch_token_to_mask=tokens_to_mask, batch_size=batch_size)
-        log_likelihood = self.__get_ll(data_loader)
-        return list(map(lambda x: -1 * x, log_likelihood))
+        return self.__get_nll(data_loader)
 
-    def __get_ll(self, data_loader):
-        """ get log likelihood with a dataloader """
+    def __get_nll(self, data_loader):
+        """ get negative log likelihood with a dataloader """
         loss_fct = nn.CrossEntropyLoss(reduction='none')
-        log_likelihood = []
+        nll = []
         with torch.no_grad():
             for encode in tqdm(data_loader):
                 encode = {k: v.to(self.device) for k, v in encode.items()}
@@ -260,13 +259,18 @@ class TransformersLM:
                 output = self.model(**encode, return_dict=True)
                 prediction_scores = output['logits']
                 loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
-                loss = loss.view(len(prediction_scores), -1)
                 loss = torch.sum(loss.view(len(prediction_scores), -1), -1)
-                log_likelihood += list(map(
+                # print(list(map(
+                #     lambda x: sum(map(lambda y: y != PAD_TOKEN_LABEL_ID, x[1])),
+                #     zip(loss.cpu().tolist(), labels.cpu().tolist())
+                # )))
+                nll += list(map(
                     lambda x: x[0]/sum(map(lambda y: y != PAD_TOKEN_LABEL_ID, x[1])),
                     zip(loss.cpu().tolist(), labels.cpu().tolist())
                 ))
-        return log_likelihood
+        print(nll)
+
+        return nll
 
     def get_embedding(self, texts: List, tokens_to_embed: List, batch_size: int = None):
         """ get embedding

alm/scoring_function.py
@@ -3,6 +3,7 @@ from typing import List
 from time import time
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
+import torch
 from . import TransformersLM
 from .data import get_dataset_prompt
 from .config_manager import ConfigManager
@@ -89,6 +90,10 @@ class RelationScorer:
             embedding_mode=embedding_mode)
         self.model_name = model
 
+    def release_cache(self):
+        if self.lm.device == "cuda":
+            torch.cuda.empty_cache()
+
     def analogy_test(self,
                      path_to_data: str,
                      batch_size: int = 4,
@@ -98,7 +103,8 @@ class RelationScorer:
                      aggregation_positive: str = 'mean',
                      aggregation_negative: str = 'none',
                      skip_scoring_prediction: bool = False,
-                     export_dir: str = './results'):
+                     export_dir: str = './results',
+                     no_inference: bool = False):
         """ relation scoring test on analogy dataset
 
         :param path_to_data:
@@ -109,6 +115,7 @@ class RelationScorer:
         :param aggregation_positive: aggregation method for positive permutations (`mean`, `max`, `min`)
         :param aggregation_negative: aggregation method for negative permutations (`mean`, `max`, `min`)
         :param skip_scoring_prediction:
+        :param no_inference: use only cached score
         :param export_dir: directory to export the result
         :return:
         """
@@ -150,6 +157,7 @@ class RelationScorer:
                 logging.info(' * load score')
                 return cached_score
             logging.info(' * run inference')
+            assert not no_inference, 'no cache found'
             if scoring_method == 'ppl':
                 return self.lm.get_perplexity(batch_data, batch_size=batch_size)
             else:
@@ -177,8 +185,6 @@ class RelationScorer:
         assert len(pred) == len(list_answer)
         accuracy = sum(map(lambda x: int(x[0] == x[1]), zip(pred, list_answer))) / len(list_answer)
         logging.info('accuracy: {}'.format(accuracy))
-
-        # save
         config.save(accuracy=accuracy, logit_pn=logit_pn, logit=logit, prediction=pred)
         logging.info('experiment completed: {} sec in total'.format(time()-start))
 
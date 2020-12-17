e5bf356514b499a85042267e86c4dd40fa6b8410
asahi417 <asahi1992ushio@gmail.com>
2020-12-17 00:28:10 +0000

    activate pmi weight

--- alm/lm_util.py
+++ alm/lm_util.py
@@ -273,11 +273,11 @@ class TransformersLM:
 
         assert len(batch_text) == len(batch_token_to_mask) == len(batch_token_to_mask_no_label)
 
-        # this can be parallelized, but due to deepcopy at DictKeeper, it may cause memory error in some machine
+        logging.info('creating data loader')
         data_dk = []
-        for x in zip(batch_text, batch_token_to_mask, batch_token_to_mask_no_label):
+        # this can be parallelized, but due to deepcopy at DictKeeper, it may cause memory error in some machine
+        for x in tqdm(zip(batch_text, batch_token_to_mask, batch_token_to_mask_no_label)):
             data_dk.append(self.encode_plus_mask(text=x[0], token_to_mask=x[1], token_to_mask_no_label=x[2]))
-
         # data_dk = list(map(
         #     lambda x: self.encode_plus_mask(text=x[0], token_to_mask=x[1], token_to_mask_no_label=x[2]),
         #     zip(batch_text, batch_token_to_mask, batch_token_to_mask_no_label)
@@ -398,7 +398,14 @@ class TransformersLM:
         :return: (`torch.utils.data.DataLoader` class, partition)
         """
         batch_size = len(batch_text) if batch_size is None else batch_size
-        data = list(map(lambda x: self.encode_plus_perplexity(x), batch_text))
+        # for x in tqdm(zip(batch_text, batch_token_to_mask, batch_token_to_mask_no_label)):
+        #     data_dk.append(self.encode_plus_mask(text=x[0], token_to_mask=x[1], token_to_mask_no_label=x[2]))
+        logging.info('creating data loader')
+        # data = list(map(lambda x: self.encode_plus_perplexity(x), batch_text))
+        data = []
+        for x in tqdm(batch_text):
+            data.append(self.encode_plus_perplexity(x))
+
         partition = get_partition(data)
 
         data_loader = torch.utils.data.DataLoader(

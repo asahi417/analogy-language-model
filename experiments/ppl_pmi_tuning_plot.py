b54d65b461552c88e255bbb52e0a501a07619ba4
asahi417 <asahi1992ushio@gmail.com>
2021-01-04 13:12:06 +0000

    clean up code

--- experiments/ppl_pmi_tuning_plot.py
+++ experiments/ppl_pmi_tuning_plot.py
@@ -6,24 +6,25 @@ export_dir = './results_ppl_pmi_tuning'
 df = pd.read_csv('./{}/summary.csv'.format(export_dir), index_col=0)
 sns.set_theme(style="darkgrid")
 
-# Line plot with 95% interval
-sns_plot = sns.lineplot(x="ppl_pmi_lambda", y="accuracy", data=df)
-sns_plot.set_xlabel("Lambda", fontsize=15)
-sns_plot.set_ylabel("Accuracy", fontsize=15)
-sns_plot.tick_params(labelsize=10)
-fig = sns_plot.get_figure()
-fig.savefig('{}/plot_mean.png'.format(export_dir))
-fig.clf()
+for i in ['ppl_pmi_lambda', 'ppl_pmi_alpha']:
+    # Line plot with 95% interval
+    sns_plot = sns.lineplot(x=i, y="accuracy", data=df)
+    sns_plot.set_xlabel("Lambda", fontsize=15)
+    sns_plot.set_ylabel("Accuracy", fontsize=15)
+    sns_plot.tick_params(labelsize=10)
+    fig = sns_plot.get_figure()
+    fig.savefig('{}/plot_mean.{}.png'.format(export_dir, i))
+    fig.clf()
 
-# Line plot with individual result
-df['aggregation_positive'] = ['P'+i.replace('p_', '') if 'p_' in i else i
-                              for i in df['aggregation_positive'].values.tolist()]
-sns_plot = sns.lineplot(x="ppl_pmi_lambda", y="accuracy", data=df, hue='aggregation_positive')
-sns_plot.set_xlabel("Lambda", fontsize=15)
-sns_plot.set_ylabel("Accuracy", fontsize=15)
-sns_plot.tick_params(labelsize=10)
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.tight_layout()
-fig = sns_plot.get_figure()
-fig.savefig('{}/plot.png'.format(export_dir))
-fig.clf()
+    # Line plot with individual result
+    df['aggregation_positive'] = ['P'+i.replace('p_', '') if 'p_' in i else i
+                                  for i in df['aggregation_positive'].values.tolist()]
+    sns_plot = sns.lineplot(x=i, y="accuracy", data=df, hue='aggregation_positive')
+    sns_plot.set_xlabel("Lambda", fontsize=15)
+    sns_plot.set_ylabel("Accuracy", fontsize=15)
+    sns_plot.tick_params(labelsize=10)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.tight_layout()
+    fig = sns_plot.get_figure()
+    fig.savefig('{}/plot.{}.png'.format(export_dir, i))
+    fig.clf()

f83eb3bc2967064a740e0497c71575d420a89400
asahi417 <asahi1992ushio@gmail.com>
2021-01-04 13:23:12 +0000

    clean up code

--- experiments/ppl_pmi_tuning_plot.py
+++ experiments/ppl_pmi_tuning_plot.py
@@ -7,8 +7,10 @@ df = pd.read_csv('./{}/summary.csv'.format(export_dir), index_col=0)
 sns.set_theme(style="darkgrid")
 
 for i, n in zip(['ppl_pmi_lambda', 'ppl_pmi_alpha'], ['Lambda', 'Alpha']):
+    tmp_df = df[df[i] == 1]
+
     # Line plot with 95% interval
-    sns_plot = sns.lineplot(x=i, y="accuracy", data=df)
+    sns_plot = sns.lineplot(x=i, y="accuracy", data=tmp_df)
     sns_plot.set_xlabel(n, fontsize=15)
     sns_plot.set_ylabel("Accuracy", fontsize=15)
     sns_plot.tick_params(labelsize=10)
@@ -19,7 +21,7 @@ for i, n in zip(['ppl_pmi_lambda', 'ppl_pmi_alpha'], ['Lambda', 'Alpha']):
     # Line plot with individual result
     df['aggregation_positive'] = ['P'+i.replace('p_', '') if 'p_' in i else i
                                   for i in df['aggregation_positive'].values.tolist()]
-    sns_plot = sns.lineplot(x=i, y="accuracy", data=df, hue='aggregation_positive')
+    sns_plot = sns.lineplot(x=i, y="accuracy", data=tmp_df, hue='aggregation_positive')
     sns_plot.set_xlabel(n, fontsize=15)
     sns_plot.set_ylabel("Accuracy", fontsize=15)
     sns_plot.tick_params(labelsize=10)
@@ -28,3 +30,14 @@ for i, n in zip(['ppl_pmi_lambda', 'ppl_pmi_alpha'], ['Lambda', 'Alpha']):
     fig = sns_plot.get_figure()
     fig.savefig('{}/plot.{}.png'.format(export_dir, i))
     fig.clf()
+
+
+result = df.pivot(index='ppl_pmi_lambda', columns='ppl_pmi_alpha', values='accuracy')
+sns_plot = sns.heatmap(result, annot=True, fmt="g", cmap='viridis')
+sns_plot.set_xlabel("Lambda", fontsize=15)
+sns_plot.set_ylabel("Alpha", fontsize=15)
+sns_plot.tick_params(labelsize=10)
+fig = sns_plot.get_figure()
+fig.savefig('{}/plot.heatmap.png'.format(export_dir))
+
+

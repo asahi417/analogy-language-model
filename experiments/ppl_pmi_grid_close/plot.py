8a769db387755807695f06ac61b6517acb301fd1
asahi417 <asahi1992ushio@gmail.com>
2021-01-04 16:04:36 +0000

    clean up code

--- experiments/ppl_pmi_grid_close/plot.py
+++ experiments/ppl_pmi_grid_close/plot.py
@@ -9,13 +9,13 @@ sns.set_theme(style="darkgrid")
 
 fig = plt.figure()
 fig.clear()
-tmp = df[df['aggregation_positive'] == i]
-result = tmp.pivot(index='ppl_pmi_lambda', columns='ppl_pmi_alpha', values='accuracy')
+result = df.pivot(index='ppl_pmi_lambda', columns='ppl_pmi_alpha', values='accuracy')
 sns_plot = sns.heatmap(result, annot=True, fmt="g", cmap='viridis', cbar=False)
 sns_plot.set_xlabel("Alpha", fontsize=15)
 sns_plot.set_ylabel("Lambda", fontsize=15)
 sns_plot.tick_params(labelsize=10)
 fig = sns_plot.get_figure()
+plt.tight_layout()
 fig.savefig('{}/plot.heatmap.{}.png'.format(export_dir, df['aggregation_positive'][0]))
 
 

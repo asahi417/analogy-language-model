a10f21b76cdfad66574d374b21fcde58e2dcc677
asahi417 <asahi1992ushio@gmail.com>
2021-01-04 15:22:03 +0000

    clean up code

--- experiments/ppl_pmi_grid_close/plot.py
+++ experiments/ppl_pmi_grid_close/plot.py
@@ -8,40 +8,8 @@ df['aggregation_positive'] = ['P'+i.replace('p_', '') if 'p_' in i else i
                                   for i in df['aggregation_positive'].values.tolist()]
 sns.set_theme(style="darkgrid")
 
-for i, n in zip(['ppl_pmi_lambda', 'ppl_pmi_alpha'], ['Lambda', 'Alpha']):
-    if i == 'ppl_pmi_lambda':
-        tmp_df = df[df['ppl_pmi_alpha'] == 1]
-    else:
-        tmp_df = df[df['ppl_pmi_lambda'] == 1]
-
-    print(tmp_df)
-
-    # Line plot with 95% interval
-    fig = plt.figure()
-    fig.clear()
-    sns_plot = sns.lineplot(x=i, y="accuracy", data=tmp_df)
-    sns_plot.set_xlabel(n, fontsize=15)
-    sns_plot.set_ylabel("Accuracy", fontsize=15)
-    sns_plot.tick_params(labelsize=10)
-    fig = sns_plot.get_figure()
-    fig.savefig('{}/plot_mean.{}.png'.format(export_dir, i))
-    fig.clear()
-
-    # Line plot with individual result
-    fig = plt.figure()
-    fig.clear()
-    sns_plot = sns.lineplot(x=i, y="accuracy", data=tmp_df, hue='aggregation_positive')
-    sns_plot.set_xlabel(n, fontsize=15)
-    sns_plot.set_ylabel("Accuracy", fontsize=15)
-    sns_plot.tick_params(labelsize=10)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    plt.tight_layout()
-    fig = sns_plot.get_figure()
-    fig.savefig('{}/plot.{}.png'.format(export_dir, i))
-    fig.clear()
 
 for i in list(set(list(df['aggregation_positive'].values))):
-
     fig = plt.figure()
     fig.clear()
     tmp = df[df['aggregation_positive'] == i]

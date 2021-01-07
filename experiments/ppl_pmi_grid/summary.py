50b23e5baee400419fb9e31b2ca33da60d83ddf5
asahi417 <asahi1992ushio@gmail.com>
2021-01-07 15:48:13 +0000

    clean up code

--- experiments/ppl_pmi_grid/summary.py
+++ experiments/ppl_pmi_grid/summary.py
@@ -7,13 +7,15 @@ import pandas as pd
 aggregation_positives = ['max', 'mean', 'min', 'p_0', 'p_1', 'p_2', 'p_3', 'p_4', 'p_5', 'p_6', 'p_7']
 aggregation_ppl_pmi = ['max', 'mean', 'min', 'p_0', 'p_1']
 export_dir = './experiments/ppl_pmi_grid/results'
+lambdas = [-2.0, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0]
+alphas = [-2.0, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0]
 
 
 def main(path_to_data, template):
     # get accuracy
     scorer = alm.RelationScorer(model='roberta-large', max_length=32)
-    for i in [-2.0, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0]:
-        for _i in [-2.0, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0]:
+    for i in lambdas:
+        for _i in alphas:
             for __i in aggregation_ppl_pmi:
                 list(map(lambda x: scorer.analogy_test(
                     scoring_method='ppl_pmi',
@@ -28,12 +30,25 @@ def main(path_to_data, template):
                     export_dir=export_dir
                     ),  aggregation_positives))
 
+
+if __name__ == '__main__':
+    # main(path_to_data='./data/sat_package_v3.jsonl', template='as-what-same')  # p_2
+    # main(path_to_data='./data/u2.jsonl', template='she-to-as')  # min
+    # main(path_to_data='./data/u4.jsonl', template='what-is-to')  # p_0
+
     # export as a csv
     index = ['model', 'path_to_data', 'scoring_method', 'template_types', 'aggregation_positive', 'ppl_pmi_lambda',
              'ppl_pmi_alpha', 'ppl_pmi_aggregation']
     df = pd.DataFrame(index=index + ['accuracy'])
 
     for i in glob('./{}/outputs/*'.format(export_dir)):
+        with open(os.path.join(i, 'config.json'), 'r') as f:
+            config = json.load(f)
+            if config['ppl_pmi_lambda'] not in lambdas:
+                continue
+            if config['ppl_pmi_alpha'] not in alphas:
+                continue
+
         with open(os.path.join(i, 'accuracy.json'), 'r') as f:
             accuracy = json.load(f)
         with open(os.path.join(i, 'config.json'), 'r') as f:
@@ -44,9 +59,3 @@ def main(path_to_data, template):
     df = df.T
     df = df.sort_values(by=index, ignore_index=True)
     df.to_csv('{}/summary.csv'.format(export_dir))
-
-
-if __name__ == '__main__':
-    # main(path_to_data='./data/sat_package_v3.jsonl', template='as-what-same')  # p_2
-    main(path_to_data='./data/u2.jsonl', template='she-to-as')  # min
-    main(path_to_data='./data/u4.jsonl', template='what-is-to')  # p_0
--- experiments/ppl_pmi_grid/summary_close.py
+++ experiments/ppl_pmi_grid/summary_close.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 export_dir = './experiments/ppl_pmi_grid/results'
 aggregation_ppl_pmi_whole = ['max', 'mean', 'min', 'p_0', 'p_1']
+lambdas = list(map(lambda x: x/10, range(10, 21)))
+alphas = list(map(lambda x: x / 10, range(-10, 1)))
 
 
 def main(path_to_data, template, aggregation_positive):
@@ -13,8 +15,8 @@ def main(path_to_data, template, aggregation_positive):
     for ppl_pmi_aggregation in aggregation_ppl_pmi_whole:
         # get accuracy
         scorer = alm.RelationScorer(model='roberta-large', max_length=32)
-        for lam in map(lambda x: x/10, range(10, 21)):
-            for alpha in map(lambda x: x / 10, range(-10, 1)):
+        for lam in lambdas:
+            for alpha in alphas:
                 scorer.analogy_test(
                     scoring_method='ppl_pmi',
                     path_to_data=path_to_data,
@@ -27,12 +29,29 @@ def main(path_to_data, template, aggregation_positive):
                     export_dir=export_dir
                 )
 
+
+if __name__ == '__main__':
+    # main(path_to_data='./data/sat_package_v3.jsonl',
+    #      template='as-what-same', aggregation_positive='p_2')
+    # main(path_to_data='./data/u2.jsonl',
+    #      template='she-to-as', aggregation_positive='p_0')
+    # main(path_to_data='./data/u4.jsonl',
+    #      template='what-is-to', aggregation_positive='p_0')
+
     # export as a csv
     index = ['model', 'path_to_data', 'scoring_method', 'template_types', 'aggregation_positive', 'ppl_pmi_lambda',
              'ppl_pmi_alpha', 'ppl_pmi_aggregation']
     df = pd.DataFrame(index=index + ['accuracy'])
 
-    for i in glob('./{}/outputs/*'.format(export_dir)):
+    for i in glob('{}/outputs/*'.format(export_dir)):
+
+        with open(os.path.join(i, 'config.json'), 'r') as f:
+            config = json.load(f)
+            if config['ppl_pmi_lambda'] not in lambdas:
+                continue
+            if config['ppl_pmi_alpha'] not in alphas:
+                continue
+
         with open(os.path.join(i, 'accuracy.json'), 'r') as f:
             accuracy = json.load(f)
         with open(os.path.join(i, 'config.json'), 'r') as f:
@@ -44,11 +63,3 @@ def main(path_to_data, template, aggregation_positive):
     df = df.sort_values(by=index, ignore_index=True)
     df.to_csv('{}/summary.close.csv'.format(export_dir))
 
-
-if __name__ == '__main__':
-    main(path_to_data='./data/sat_package_v3.jsonl',
-         template='as-what-same', aggregation_positive='p_2')
-    main(path_to_data='./data/u2.jsonl',
-         template='she-to-as', aggregation_positive='p_0')
-    main(path_to_data='./data/u4.jsonl',
-         template='what-is-to', aggregation_positive='p_0')

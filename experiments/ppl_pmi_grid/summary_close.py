3ff6efa624d7567875f10aa56bb39b9982b28e19
asahi417 <asahi1992ushio@gmail.com>
2021-01-07 15:15:59 +0000

    clean up code

--- experiments/ppl_pmi_grid/summary_close.py
+++ experiments/ppl_pmi_grid/summary_close.py
@@ -5,24 +5,27 @@ from glob import glob
 import pandas as pd
 
 export_dir = './experiments/ppl_pmi_grid/results'
+aggregation_ppl_pmi_whole = ['max', 'mean', 'min', 'p_0', 'p_1']
 
 
-def main(path_to_data, template, aggregation_positive, ppl_pmi_aggregation):
-    # get accuracy
-    scorer = alm.RelationScorer(model='roberta-large', max_length=32)
-    for lam in map(lambda x: x/10, range(10, 21)):
-        for alpha in map(lambda x: x / 10, range(-10, 1)):
-            scorer.analogy_test(
-                scoring_method='ppl_pmi',
-                path_to_data=path_to_data,
-                ppl_pmi_aggregation=ppl_pmi_aggregation,
-                template_types=[template],
-                aggregation_positive=aggregation_positive,
-                ppl_pmi_lambda=lam,
-                ppl_pmi_alpha=alpha,
-                no_inference=True,
-                export_dir=export_dir
-            )
+def main(path_to_data, template, aggregation_positive):
+
+    for ppl_pmi_aggregation in aggregation_ppl_pmi_whole:
+        # get accuracy
+        scorer = alm.RelationScorer(model='roberta-large', max_length=32)
+        for lam in map(lambda x: x/10, range(10, 21)):
+            for alpha in map(lambda x: x / 10, range(-10, 1)):
+                scorer.analogy_test(
+                    scoring_method='ppl_pmi',
+                    path_to_data=path_to_data,
+                    ppl_pmi_aggregation=ppl_pmi_aggregation,
+                    template_types=[template],
+                    aggregation_positive=aggregation_positive,
+                    ppl_pmi_lambda=lam,
+                    ppl_pmi_alpha=alpha,
+                    no_inference=True,
+                    export_dir=export_dir
+                )
 
     # export as a csv
     index = ['model', 'path_to_data', 'scoring_method', 'template_types', 'aggregation_positive', 'ppl_pmi_lambda',
@@ -44,8 +47,8 @@ def main(path_to_data, template, aggregation_positive, ppl_pmi_aggregation):
 
 if __name__ == '__main__':
     main(path_to_data='./data/sat_package_v3.jsonl',
-         template='as-what-same', aggregation_positive='p_2', ppl_pmi_aggregation='mean')
+         template='as-what-same', aggregation_positive='p_2')
     main(path_to_data='./data/u2.jsonl',
-         template='she-to-as', aggregation_positive='p_0', ppl_pmi_aggregation='mean')
+         template='she-to-as', aggregation_positive='p_0')
     main(path_to_data='./data/u4.jsonl',
-         template='what-is-to', aggregation_positive='p_0', ppl_pmi_aggregation='mean')
+         template='what-is-to', aggregation_positive='p_0')

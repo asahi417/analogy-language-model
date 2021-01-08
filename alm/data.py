106e953041fe42fe18cd94d40434af57e0384e62
asahi417 <asahi1992ushio@gmail.com>
2021-01-08 00:41:15 +0000

    clean up code

--- alm/data.py
+++ alm/data.py
@@ -129,7 +129,8 @@ class AnalogyData:
 
     def insert_score(self, score_positive: List, score_negative: List = None):
         """ restore the nested structure from a flatten list """
-
+        print('pos: {}'.format(len(score_positive)))
+        print('neg: {}'.format(len(score_negative)))
         list_placeholder = list(map(
             lambda x: list(map(
                 lambda y: (
@@ -143,8 +144,11 @@ class AnalogyData:
 
         for n_q, n_o, n_perm in self.structure_id_pos:
             list_placeholder[n_q][n_o][0][n_perm] = list_score_pos.pop(0)
+
         if score_negative is not None and self.structure_id_neg is not None:
             list_score_neg = score_negative.copy()
             for n_q, n_o, n_perm in self.structure_id_neg:
                 list_placeholder[n_q][n_o][1][n_perm] = list_score_neg.pop(0)
+        print('pos: {}'.format(len(score_positive)))
+        print('neg: {}'.format(len(score_negative)))
         return list_placeholder

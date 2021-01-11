d08ac045a2bd25707da24d930a64c8294e0a572e
asahi417 <asahi1992ushio@gmail.com>
2021-01-11 23:18:23 +0000

    fix negative permutations

--- alm/scoring_function.py
+++ alm/scoring_function.py
@@ -131,7 +131,7 @@ class RelationScorer:
 
         # restore the nested structure
         logging.info('restore batch structure')
-        print(len(score_pos), len(score_neg))
+        print(len(score_pos), score_neg)
         score = data_instance.insert_score(score_pos, score_neg)
 
         ##############

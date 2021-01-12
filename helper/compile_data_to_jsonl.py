d8c941d6fdfe959ec2ed271eef8a408fc78d7ef2
asahi417 <asahi1992ushio@gmail.com>
2021-01-12 14:43:49 +0000

    fix dataset

--- helper/compile_data_to_jsonl.py
+++ helper/compile_data_to_jsonl.py
@@ -42,14 +42,18 @@ def process_single_entry_sat(single_entry: str):
 
 
 def process_single_entry(single_entry: str, level: str):
-    lines = [re.sub(r'\s\Z', '', t).replace('\u0301', '').replace('\\', '') for t in single_entry.split('\n')]
-    lines = list(filter(lambda x: len(x) > 0, lines))
+    lines_org = [re.sub(r'\s+\Z', '', t).replace('\u0301', '').replace('\\', '') for t in single_entry.split('\n')]
+    lines = list(filter(lambda x: len(x) > 0, lines_org))
     target = (lines[0].split(' ')[1].lower(), lines[0].split(' ')[-1].lower())
     if target[0] == target[1]:
         target = target[0].split(':')
     target = list(map(lambda x: x.replace(':', ''), target))
     answer_id = ALPHABET_LIST.index(lines[-1])
     choice_list = [(__i.split(' ')[1].replace(':', ''), __i.split(' ')[-1].replace(':', '')) for __i in lines[1:-1]]
+    for a, b in choice_list:
+        if len(a) == 0 or len(b) == 0:
+            raise ValueError(lines_org)
+
     return {"stem": target, "answer": answer_id, "choice": choice_list, "prefix": level}
 
 
@@ -70,6 +74,8 @@ def check_validity(json_data):
         a = i['answer']
         choice = []
         for n, (c_h, c_t) in enumerate(i['choice']):
+            if len(c_h) == 0 or len(c_t) == 0:
+                raise ValueError(i)
             if c_h in i['stem'] or c_t in i['stem']:
                 print("found duplication: {} \n {}".format((c_h, c_t), i))
                 if a == n:

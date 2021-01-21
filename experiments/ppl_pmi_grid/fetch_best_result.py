cf2f3c5eaf9584c403f6e332d735f0c0912fcabc
asahi417 <asahi1992ushio@gmail.com>
2021-01-21 11:49:22 +0000

    search best config

--- experiments/ppl_pmi_grid/fetch_best_result.py
+++ experiments/ppl_pmi_grid/fetch_best_result.py
@@ -5,9 +5,10 @@ from glob import glob
 import pandas as pd
 
 
-def safe_open(_file):
+def safe_open(_file, keys):
     with open(_file, 'r') as f:
-        return json.load(f)
+        d = json.load(f)
+        return {k: d[k] for k in keys}
 
 
 for i in ['sat_package_v3', 'u2_raw', 'u4_raw']:
@@ -16,8 +17,11 @@ for i in ['sat_package_v3', 'u2_raw', 'u4_raw']:
     best = json.loads(df.iloc[0].T.to_json())
     accuracy = best.pop('accuracy')
     print("{}: {}".format(i, accuracy))
-    ex_configs = {i: safe_open(i) for i in glob('./experiments/ppl_pmi_grid/results/outputs/*/config.json')}
+    ex_configs = {
+        i: safe_open(i, best.keys()) for i in glob('./experiments/ppl_pmi_grid/results/outputs/*/config.json')}
     # check duplication
+    print(list(ex_configs.items())[0])
+    print(best)
     same_config = list(filter(lambda x: x[1] == best, ex_configs.items()))
     print(same_config)
 

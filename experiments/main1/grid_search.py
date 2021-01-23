2995c3c6c87eb3a9bcf9a1b2fa713d25dd57cc24
asahi417 <asahi1992ushio@gmail.com>
2021-01-23 21:50:06 +0000

    search best config

--- experiments/main1/grid_search.py
+++ experiments/main1/grid_search.py
@@ -3,15 +3,15 @@ import alm
 all_templates = ['is-to-what', 'is-to-as', 'rel-same', 'what-is-to', 'she-to-as', 'as-what-same']
 methods = ['ppl_tail_masked', 'ppl_head_masked', 'ppl', 'embedding_similarity', 'ppl_pmi', 'ppl_add_masked']  # 'pmi'
 data = ['sat', 'u2', 'u4', 'google', 'bats']
-models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 512), ('bert-large-cased', 64, 512), ('gpt2-large', 32, 512)]
+models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 64, 512), ('gpt2-large', 32, 512)]
 
 positive_permutation_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4',
                                     'index_5', 'index_6', 'index_7']
 pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6',
                    'index_7', 'index_8', 'index_9', 'index_10', 'index_11']
 ppl_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
-export_prefix = 'main'
-#
+export_prefix = 'main1'
+
 for _model, _max_length, _batch in models:
     for scoring_method in methods:
         if scoring_method in ['pmi', 'ppl_tail_masked', 'ppl_head_masked', 'ppl_add_masked'] and 'gpt' in _model:

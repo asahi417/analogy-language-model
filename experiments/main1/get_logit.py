674d6c56ee34038ea8a78f31ed6b2e7aecc8f3b9
asahi417 <asahi1992ushio@gmail.com>
2021-01-23 21:51:43 +0000

    search best config

--- experiments/main1/get_logit.py
+++ experiments/main1/get_logit.py
@@ -1,7 +1,8 @@
 import alm
 
 all_templates = ['is-to-what', 'is-to-as', 'rel-same', 'what-is-to', 'she-to-as', 'as-what-same']
-methods = ['ppl_tail_masked', 'ppl_head_masked', 'ppl_add_masked', 'ppl', 'embedding_similarity', 'ppl_pmi'] #, 'pmi']
+# methods = ['ppl_tail_masked', 'ppl_head_masked', 'ppl_add_masked', 'ppl', 'embedding_similarity', 'ppl_pmi'] #, 'pmi']
+methods = ['pmi']
 data = ['sat', 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 64, 512), ('gpt2-large', 32, 512)]
 

e4ea01309c444c04b85ed3d034399eedd11b3d41
asahi417 <asahi1992ushio@gmail.com>
2021-01-24 18:28:55 +0000

    search best config

--- experiments/main2/run_test_set.py
+++ experiments/main2/run_test_set.py
@@ -9,11 +9,17 @@ df = alm.get_report(export_prefix=export_prefix)
 
 for i in data:
     tmp_df = df[df.data == i]
-    tmp_df = tmp_df.sort_values(by='accuracy', ascending=False).iloc[0]
-    config = json.loads(tmp_df.to_json())
-    logging.info("RUN TEST:\n - data: {} \n - validation accuracy: {} ".format(i, config.pop('accuracy')))
-    scorer = alm.RelationScorer(model=config.pop('model'), max_length=config.pop('max_length'))
-    scorer.analogy_test(test=True, batch_size=512, **config)
-    scorer.release_cache()
+    val_accuracy = tmp_df.sort_values(by='accuracy', ascending=False).head(1)['accuracy'].values[0]
+    logging.info("RUN TEST:\n - data: {} \n - validation accuracy: {} ".format(i, val_accuracy))
+    best_configs = tmp_df[tmp_df['accuracy'] == val_accuracy]
+    logging.info("find {} configs with same accuracy".format(best_configs))
+    for n, tmp_df in best_configs.iterrows():
+        config = json.loads(tmp_df.to_json())
+        config.pop('accuracy')
+        scorer = alm.RelationScorer(model=config.pop('model'), max_length=config.pop('max_length'))
+        scorer.analogy_test(test=True,
+                            batch_size=128 if 'gpt2-xl' == scorer.model_name else 512,
+                            **config)
+        scorer.release_cache()
 
 alm.export_report(export_prefix=export_prefix, test=True)

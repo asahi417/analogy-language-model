dcb480f985f7fce59992fa72c6f97b7a1a776fdc
asahi417 <asahi1992ushio@gmail.com>
2021-01-25 15:14:20 +0000

    search best config

--- experiments/main2/export_test_prediction.py
+++ experiments/main2/export_test_prediction.py
@@ -15,10 +15,6 @@ for i in data:
     logging.info("RUN TEST:\n - data: {} \n - validation accuracy: {} ".format(i, val_accuracy))
     best_configs = tmp_df[tmp_df['accuracy'] == val_accuracy]
     logging.info("find {} configs with same accuracy".format(len(best_configs)))
-    accuracy_list = [tmp_df['accuracy'] for n, tmp_df in best_configs.iterrows()]
-    print(accuracy_list)
-    logging.info("min accuracy: {}".format(min(accuracy_list)))
-    logging.info("max accuracy: {}".format(max(accuracy_list)))
     config = json.loads(best_configs.iloc[0].to_json())
     logging.info("use the first one: {} accuracy".format(config.pop('accuracy')))
     scorer = alm.RelationScorer(model=config.pop('model'), max_length=config.pop('max_length'))

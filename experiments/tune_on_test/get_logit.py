dee9f4b084aa3b3b6b169b845fd02801c27b6076
asahi417 <asahi1992ushio@gmail.com>
2021-01-26 15:22:00 +0000

    search best config

---
+++ experiments/tune_on_test/get_logit.py
@@ -0,0 +1,22 @@
+import alm
+
+all_templates = ['is-to-what', 'is-to-as', 'rel-same', 'what-is-to', 'she-to-as', 'as-what-same']
+data = ['sat', 'u2', 'u4', 'google', 'bats']
+models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128)]
+
+for _model, _max_length, _batch in models:
+    scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+    for _data in data:
+        for _temp in all_templates:
+            scorer.analogy_test(
+                test=True,
+                scoring_method='ppl_pmi',
+                data=_data,
+                template_type=_temp,
+                batch_size=_batch,
+                negative_permutation=True,
+                skip_scoring_prediction=True
+            )
+            scorer.release_cache()
+
+

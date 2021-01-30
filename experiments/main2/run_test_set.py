91158799b09bcac3db549e4218e9f8fc6f4fd968
asahi417 <asahi1992ushio@gmail.com>
2021-01-30 18:57:43 +0000

    fix cache directory

--- experiments/main2/run_test_set.py
+++ experiments/main2/run_test_set.py
@@ -5,8 +5,10 @@ from itertools import product
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 import alm
 
-data = ['sat', 'u2', 'u4', 'google', 'bats']
-models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
+# data = ['sat', 'u2', 'u4', 'google', 'bats']
+# models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
+data = ['bats']
+models = [('bert-large-cased', 32, 1024)]
 # models = [('bert-large-cased', 32, 512)]
 export_prefix = 'main2'
 df = alm.get_report(export_prefix=export_prefix)
@@ -29,6 +31,7 @@ for i, m in product(data, models):
                             batch_size=_batch,
                             **config)
         scorer.release_cache()
-        break  # reduce the time
+        # if m == 'bert-large-cased':
+        #     break  # reduce the time
 
 alm.export_report(export_prefix=export_prefix, test=True)

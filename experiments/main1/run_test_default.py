85642b524b1565c96e463b1400a4a90d82f828b3
asahi417 <asahi1992ushio@gmail.com>
2021-02-01 10:30:21 +0000

    fix pmi

--- experiments/main1/run_test_default.py
+++ experiments/main1/run_test_default.py
@@ -11,17 +11,17 @@ models = [('roberta-large', 32, 512), ('bert-large-cased', 32, 1024), ('gpt2-xl'
 
 export_prefix = 'main1.default'
 
-for i, m in product(data, models):
-    _model, _len, _batch = m
-    scorer = alm.RelationScorer(model=_model, max_length=_len)
-    scorer.analogy_test(test=True,
-                        template_type='is-to-as',
-                        export_prefix=export_prefix,
-                        batch_size=_batch,
-                        data=i,
-                        scoring_method='ppl',
-                        positive_permutation_aggregation='index_0')
-    scorer.release_cache()
+# for i, m in product(data, models):
+#     _model, _len, _batch = m
+#     scorer = alm.RelationScorer(model=_model, max_length=_len)
+#     scorer.analogy_test(test=True,
+#                         template_type='is-to-as',
+#                         export_prefix=export_prefix,
+#                         batch_size=_batch,
+#                         data=i,
+#                         scoring_method='ppl',
+#                         positive_permutation_aggregation='index_0')
+#     scorer.release_cache()
 
 for i, m in product(data, models):
     _model, _len, _batch = m

7ba184a3a9c1f9aee3b176446b97fece28d0c189
asahi417 <asahi1992ushio@gmail.com>
2021-02-01 23:10:17 +0000

    fix pmi

--- experiments/main2/plot_heatmap.py
+++ experiments/main2/plot_heatmap.py
@@ -2,14 +2,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import alm
 import os
+import json
 from itertools import product
+
 plt.rcParams.update({"text.usetex": True, "font.family": "sans-serif", "font.sans-serif": ["Helvetica"]})
 
 os.makedirs('./experiments_results/summary/main2_figure', exist_ok=True)
 export_prefix = 'main2'
 df = alm.get_report(export_prefix=export_prefix)
+# g = df.groupby(['data'])
+# mean_acc = json.loads(g.accuracy.mean().to_json())
+# df['accuracy_mean'] = df['data'].apply(lambda x: mean_acc[x])
+# df['accuracy'] = df['accuracy'] - df['accuracy_mean']
 df['accuracy'] = df['accuracy'] * 100
-df['accuracy'] = df['accuracy'].round(5)
+df['accuracy'] = df['accuracy'].round(1)
 data = ['sat', 'u2', 'u4', 'google', 'bats']
 model = ['roberta-large', 'gpt2-xl']
 big_group = df.groupby(['data', 'model', 'ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.max()
@@ -36,14 +42,15 @@ def plot(d, m, accuracy):
     plt.close()
 
 
-plot('all', 'all', df.groupby(['ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.max())
-# for m_ in model:
-#     plot('all', m_, df.groupby(['model', 'ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.max()[m_])
-#
-# for d_ in data:
-#     plot(d_, 'all', df.groupby(['data', 'ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.max()[d_])
-#
-# for d_, m_ in product(data, model):
-#     plot(d_, m_, big_group[d_][m_])
+# plot('all', 'all', df.groupby(['ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.max())
+plot('all', 'all', df.groupby(['ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.mean())
+for m_ in model:
+    plot('all', m_, df.groupby(['model', 'ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.mean()[m_])
+
+for d_ in data:
+    plot(d_, 'all', df.groupby(['data', 'ppl_pmi_alpha', 'negative_permutation_weight']).accuracy.mean()[d_])
+
+for d_, m_ in product(data, model):
+    plot(d_, m_, big_group[d_][m_])
 
 

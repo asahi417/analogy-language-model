11be41e9fd6bc0c5e405dba793f54c0e02fce962
asahi417 <asahi1992ushio@gmail.com>
2021-02-08 16:07:00 +0000

    cleanupdatra

--- experiments/main1/grid_search.py
+++ experiments/main1/grid_search.py
@@ -5,12 +5,13 @@ data = ['sat', 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
 methods = ['ppl_tail_masked', 'ppl_head_masked', 'ppl', 'embedding_similarity', 'ppl_pmi', 'ppl_add_masked', 'pmi']
 
-positive_permutation_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4',
-                                    'index_5', 'index_6', 'index_7']
+positive_permutation_aggregation = [
+    'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7']
 pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6',
                    'index_7', 'index_8', 'index_9', 'index_10', 'index_11']
 ppl_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
 export_prefix = 'main1'
+ppl_pmi_marginal_version = [True, False]
 
 for _model, _max_length, _batch in models:
     for scoring_method in methods:
@@ -30,7 +31,9 @@ for _model, _max_length, _batch in models:
                 )
                 if scoring_method == 'ppl_pmi':
                     shared['ppl_pmi_aggregation'] = ppl_pmi_aggregation
-                if scoring_method == 'pmi':
+                    for v in ppl_pmi_marginal_version:
+                        scorer.analogy_test(ppl_pmi_marginal_version=v, **shared)
+                elif scoring_method == 'pmi':
                     for i in pmi_aggregation:
                         scorer.analogy_test(pmi_aggregation=i, **shared)
                         scorer.release_cache()

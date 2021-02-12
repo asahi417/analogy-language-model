a8c321faf3ca935d388b2967f6ef32fe13a50dd7
asahi417 <asahi1992ushio@gmail.com>
2021-02-12 18:11:28 +0000

    fix

--- experiments/main3/merge_summary.py
+++ experiments/main3/merge_summary.py
@@ -9,9 +9,6 @@ df_test = df_test.sort_values(by=list(df_val.columns)).reindex()
 accuracy_val = df_val.pop('accuracy')
 accuracy_test = df_test.pop('accuracy')
 
-print(df_val.shape)
-print(df_test.shape)
-check = (df_val != df_test).values.sum()
-assert check == 0, '{}'.format(check)
+assert df_val.shape == df_test.shape
 df_test['accuracy_validation'] = accuracy_val
-accuracy_test.to_csv('./experiments_results/summary/main3.test.csv', index_col=0)
+df_test.to_csv('./experiments_results/summary/main3.test.csv', index_col=0)

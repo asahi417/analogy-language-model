c1eccf795dd4b82b5eefbee1e5aa3ae4db088e49
asahi417 <asahi1992ushio@gmail.com>
2021-02-12 18:58:44 +0000

    fix

--- experiments/main3/merge_summary.py
+++ experiments/main3/merge_summary.py
@@ -7,12 +7,12 @@ df_test = alm.get_report(export_prefix='main3', test=True)
 df_val = df_val.sort_values(by=list(df_val.columns))
 df_test = df_test.sort_values(by=list(df_val.columns))
 
-accuracy_val = df_val.pop('accuracy')
-accuracy_test = df_test.pop('accuracy')
+accuracy_val = df_val.pop('accuracy').to_numpy()
+accuracy_test = df_test.pop('accuracy').to_numpy()
 assert df_val.shape == df_test.shape
 
-df_test['accuracy_validation'] = accuracy_val.to_numpy()
-df_test['accuracy_test'] = accuracy_test.to_numpy()
+df_test['accuracy_validation'] = accuracy_val
+df_test['accuracy_test'] = accuracy_test
 
 df_test['accuracy'] = (accuracy_val * 37 + accuracy_test * 337)/(37 + 337)
 df_test = df_test.sort_values(by=['accuracy'], ascending=False)

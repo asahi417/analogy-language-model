5287bf1aa7072c944527df663773d8a1b7e72c53
asahi417 <asahi1992ushio@gmail.com>
2021-02-22 00:43:03 +0000

    init

--- tmp.py
+++ tmp.py
@@ -8,13 +8,19 @@ Remove pmi_lambda -> pmi_feldman_lambda
 """
 import shutil
 import json
+import os
 from glob import glob
 
 
+def move(a, b):
+    if os.path.exists(a):
+        shutil.move(a, b)
+
+
 for i in glob('./experiments_results/logit/*/*'):
-    shutil.move('{}/pmi'.format(i), '{}/pmi_feldman'.format(i))
-    shutil.move('{}/ppl_hyp'.format(i), '{}/ppl_hypothesis_bias'.format(i))
-    shutil.move('{}/ppl_pmi'.format(i), '{}/ppl_based_pmi'.format(i))
+    move('{}/pmi'.format(i), '{}/pmi_feldman'.format(i))
+    move('{}/ppl_hyp'.format(i), '{}/ppl_hypothesis_bias'.format(i))
+    move('{}/ppl_pmi'.format(i), '{}/ppl_based_pmi'.format(i))
 
 for i in glob('./experiments_results/logit/*/*/*/config.json'):
     if 'pmi_feldman' in i:

d40ef9d5d67d7e4858a210897ae59bed68472a08
asahi417 <asahi1992ushio@gmail.com>
2021-02-24 19:13:49 +0000

    init

--- experiments/experiment_ppl_variants.py
+++ experiments/experiment_ppl_variants.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import alm
 
 all_templates = ['is-to-what', 'is-to-as', 'rel-same', 'what-is-to', 'she-to-as', 'as-what-same']
-data = ['sat', 'u2', 'u4', 'google', 'bats']
+data = ['sat']  #, 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 256), ('bert-large-cased', 32, 1024)]
 scoring_method = ['ppl_hypothesis_bias', 'ppl_marginal_bias', 'ppl_based_pmi']
 

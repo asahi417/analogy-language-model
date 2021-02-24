0dc40762006a466933426054a44395ffe7f3d8c6
asahi417 <asahi1992ushio@gmail.com>
2021-02-24 22:43:31 +0000

    init

--- experiments/experiment_scoring_comparison.py
+++ experiments/experiment_scoring_comparison.py
@@ -11,66 +11,66 @@ data = ['sat', 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
 
 
-logging.info('################################################')
-logging.info('# Run LM inference to get logit (on valid set) #')
-logging.info('################################################')
-no_inference = True
-for _model, _max_length, _batch in models:
-    for scoring_method in methods:
-        if 'gpt' in _model and scoring_method in methods_mlm:
-            continue
-        scorer = alm.RelationScorer(model=_model, max_length=_max_length)
-        for _data in data:
-            for _temp in all_templates:
-                scorer.analogy_test(
-                    scoring_method=scoring_method,
-                    data=_data,
-                    template_type=_temp,
-                    batch_size=_batch,
-                    no_inference=no_inference,
-                    skip_scoring_prediction=True)
-                scorer.release_cache()
-
-
-logging.info('#######################################################')
-logging.info('# Get prediction on each configuration (on valid set) #')
-logging.info('#######################################################')
-methods += ['ppl_add_masked', 'ppl_marginal_bias', 'ppl_hypothesis_bias']
-positive_permutation_aggregation = [
-    'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7'
-]
-pmi_feldman_aggregation = [
-    'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7',
-    'index_8', 'index_9', 'index_10', 'index_11'
-]
-ppl_based_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
-export_prefix = 'experiment.scoring_comparison'
-no_inference = True
-for _model, _max_length, _batch in models:
-    for scoring_method in methods:
-        if 'gpt' in _model and scoring_method in methods_mlm:
-            continue
-        scorer = alm.RelationScorer(model=_model, max_length=_max_length)
-        for _data in data:
-            for _temp in all_templates:
-                shared = dict(
-                    scoring_method=scoring_method,
-                    data=_data,
-                    template_type=_temp,
-                    batch_size=_batch,
-                    export_prefix=export_prefix,
-                    no_inference=no_inference,
-                    positive_permutation_aggregation=positive_permutation_aggregation,
-                    ppl_based_pmi_aggregation=ppl_based_pmi_aggregation
-                )
-                if scoring_method == 'pmi_feldman':
-                    for i in pmi_feldman_aggregation:
-                        scorer.analogy_test(pmi_feldman_aggregation=i, **shared)
-                        scorer.release_cache()
-                else:
-                    scorer.analogy_test(**shared)
-                scorer.release_cache()
-alm.export_report(export_prefix=export_prefix)
+# logging.info('################################################')
+# logging.info('# Run LM inference to get logit (on valid set) #')
+# logging.info('################################################')
+# no_inference = True
+# for _model, _max_length, _batch in models:
+#     for scoring_method in methods:
+#         if 'gpt' in _model and scoring_method in methods_mlm:
+#             continue
+#         scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+#         for _data in data:
+#             for _temp in all_templates:
+#                 scorer.analogy_test(
+#                     scoring_method=scoring_method,
+#                     data=_data,
+#                     template_type=_temp,
+#                     batch_size=_batch,
+#                     no_inference=no_inference,
+#                     skip_scoring_prediction=True)
+#                 scorer.release_cache()
+#
+#
+# logging.info('#######################################################')
+# logging.info('# Get prediction on each configuration (on valid set) #')
+# logging.info('#######################################################')
+# methods += ['ppl_add_masked', 'ppl_marginal_bias', 'ppl_hypothesis_bias']
+# positive_permutation_aggregation = [
+#     'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7'
+# ]
+# pmi_feldman_aggregation = [
+#     'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7',
+#     'index_8', 'index_9', 'index_10', 'index_11'
+# ]
+# ppl_based_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
+# export_prefix = 'experiment.scoring_comparison'
+# no_inference = True
+# for _model, _max_length, _batch in models:
+#     for scoring_method in methods:
+#         if 'gpt' in _model and scoring_method in methods_mlm:
+#             continue
+#         scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+#         for _data in data:
+#             for _temp in all_templates:
+#                 shared = dict(
+#                     scoring_method=scoring_method,
+#                     data=_data,
+#                     template_type=_temp,
+#                     batch_size=_batch,
+#                     export_prefix=export_prefix,
+#                     no_inference=no_inference,
+#                     positive_permutation_aggregation=positive_permutation_aggregation,
+#                     ppl_based_pmi_aggregation=ppl_based_pmi_aggregation
+#                 )
+#                 if scoring_method == 'pmi_feldman':
+#                     for i in pmi_feldman_aggregation:
+#                         scorer.analogy_test(pmi_feldman_aggregation=i, **shared)
+#                         scorer.release_cache()
+#                 else:
+#                     scorer.analogy_test(**shared)
+#                 scorer.release_cache()
+# alm.export_report(export_prefix=export_prefix)
 
 
 logging.info('#######################################################################')
@@ -115,13 +115,10 @@ export_prefix = 'experiment.scoring_comparison.default'
 for i, m in product(data, models):
     _model, _len, _batch = m
     scorer = alm.RelationScorer(model=_model, max_length=_len)
-    val_accuracy = scorer.analogy_test(batch_size=_batch, data=i, test=False, **shared)
-    assert len(val_accuracy) == 0
     scorer.analogy_test(
         no_inference=no_inference,
         batch_size=_batch,
         data=i,
-        val_accuracy=val_accuracy,
         export_prefix=export_prefix,
         test=True,
         **shared)

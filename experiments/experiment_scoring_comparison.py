34cbf53863cbe2e1bc1cf669f9e4c1df55996bc3
asahi417 <asahi1992ushio@gmail.com>
2021-02-24 22:07:26 +0000

    init

--- experiments/experiment_scoring_comparison.py
+++ experiments/experiment_scoring_comparison.py
@@ -10,26 +10,26 @@ methods = ['pmi_feldman', 'embedding_similarity', 'ppl', 'ppl_based_pmi', 'ppl_h
 data = ['sat', 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
 
-
-logging.info('################################################')
-logging.info('# Run LM inference to get logit (on valid set) #')
-logging.info('################################################')
-no_inference = True
-for _model, _max_length, _batch in models:
-    for scoring_method in methods:
-        if 'gpt' in _model and scoring_method in methods_mlm:
-            continue
-        scorer = alm.RelationScorer(model=_model, max_length=_max_length)
-        for _data in data:
-            for _temp in all_templates:
-                scorer.analogy_test(
-                    scoring_method=scoring_method,
-                    data=_data,
-                    template_type=_temp,
-                    batch_size=_batch,
-                    no_inference=no_inference,
-                    skip_scoring_prediction=True)
-                scorer.release_cache()
+#
+# logging.info('################################################')
+# logging.info('# Run LM inference to get logit (on valid set) #')
+# logging.info('################################################')
+# no_inference = True
+# for _model, _max_length, _batch in models:
+#     for scoring_method in methods:
+#         if 'gpt' in _model and scoring_method in methods_mlm:
+#             continue
+#         scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+#         for _data in data:
+#             for _temp in all_templates:
+#                 scorer.analogy_test(
+#                     scoring_method=scoring_method,
+#                     data=_data,
+#                     template_type=_temp,
+#                     batch_size=_batch,
+#                     no_inference=no_inference,
+#                     skip_scoring_prediction=True)
+#                 scorer.release_cache()
 
 
 logging.info('#######################################################')

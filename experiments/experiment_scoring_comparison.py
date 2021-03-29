aec78153e204d6b2fcc17cb66ed3ab2da7f0f1a0
asahi417 <asahi1992ushio@gmail.com>
2021-03-29 16:26:19 +0100

    init

--- experiments/experiment_scoring_comparison.py
+++ experiments/experiment_scoring_comparison.py
@@ -12,118 +12,124 @@ data = ['sat', 'u2', 'u4', 'google', 'bats']
 models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 128), ('bert-large-cased', 32, 1024)]
 
 
-logging.info('################################################')
-logging.info('# Run LM inference to get logit (on valid set) #')
-logging.info('################################################')
-no_inference = False
-for _model, _max_length, _batch in models:
-    for scoring_method in methods:
-        if 'gpt' in _model and scoring_method in methods_mlm:
+SKIP_INFERENCE = True
+SKIP_GRID_SEARCH = True
+SKIP_TEST = True
+SKIP_DEFAULT = True
+
+if not SKIP_INFERENCE:
+    logging.info('################################################')
+    logging.info('# Run LM inference to get logit (on valid set) #')
+    logging.info('################################################')
+    no_inference = False
+    for _model, _max_length, _batch in models:
+        for scoring_method in methods:
+            if 'gpt' in _model and scoring_method in methods_mlm:
+                continue
+            scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+            for _data in data:
+                for _temp in all_templates:
+                    scorer.analogy_test(
+                        scoring_method=scoring_method,
+                        data=_data,
+                        template_type=_temp,
+                        batch_size=_batch,
+                        no_inference=no_inference,
+                        skip_scoring_prediction=True)
+                    scorer.release_cache()
+
+if not SKIP_GRID_SEARCH:
+    logging.info('#######################################################')
+    logging.info('# Get prediction on each configuration (on valid set) #')
+    logging.info('#######################################################')
+    methods += ['ppl_add_masked', 'ppl_marginal_bias', 'ppl_hypothesis_bias']
+    positive_permutation_aggregation = [
+        'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7'
+    ]
+    pmi_feldman_aggregation = [
+        'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7',
+        'index_8', 'index_9', 'index_10', 'index_11'
+    ]
+    ppl_based_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
+    export_prefix = 'experiment.scoring_comparison'
+    no_inference = False
+    for _model, _max_length, _batch in models:
+        for scoring_method in methods:
+            if 'gpt' in _model and scoring_method in methods_mlm:
+                continue
+            scorer = alm.RelationScorer(model=_model, max_length=_max_length)
+            for _data in data:
+                for _temp in all_templates:
+                    shared = dict(
+                        scoring_method=scoring_method,
+                        data=_data,
+                        template_type=_temp,
+                        batch_size=_batch,
+                        export_prefix=export_prefix,
+                        no_inference=no_inference,
+                        positive_permutation_aggregation=positive_permutation_aggregation,
+                        ppl_based_pmi_aggregation=ppl_based_pmi_aggregation
+                    )
+                    if scoring_method == 'pmi_feldman':
+                        for i in pmi_feldman_aggregation:
+                            scorer.analogy_test(pmi_feldman_aggregation=i, **shared)
+                            scorer.release_cache()
+                    else:
+                        scorer.analogy_test(**shared)
+                    scorer.release_cache()
+    alm.export_report(export_prefix=export_prefix)
+
+if not SKIP_TEST:
+    logging.info('#######################################################################')
+    logging.info('# get test accuracy on each combination of model and scoring function #')
+    logging.info('#######################################################################')
+    no_inference = False
+    export_prefix = 'experiment.scoring_comparison'
+    df = alm.get_report(export_prefix=export_prefix)
+    for i, m, s in product(data, models, methods):
+
+        _model, _len, _batch = m
+        if 'gpt' in _model and s in methods_mlm:
             continue
-        scorer = alm.RelationScorer(model=_model, max_length=_max_length)
-        for _data in data:
-            for _temp in all_templates:
-                scorer.analogy_test(
-                    scoring_method=scoring_method,
-                    data=_data,
-                    template_type=_temp,
-                    batch_size=_batch,
-                    no_inference=no_inference,
-                    skip_scoring_prediction=True)
-                scorer.release_cache()
+        tmp_df = df[df.data == i]
+        tmp_df = tmp_df[tmp_df.model == _model]
+        tmp_df = tmp_df[tmp_df.scoring_method == s]
+        val_accuracy = tmp_df.sort_values(by='accuracy', ascending=False).head(1)['accuracy'].values[0]
+        logging.info("RUN TEST:\n - data: {} \n - lm: {} \n - score: {} \n - validation accuracy: {} ".format(
+            i, _model, s, val_accuracy))
+        best_configs = tmp_df[tmp_df['accuracy'] == val_accuracy]
+        logging.info("find {} configs with same accuracy".format(len(best_configs)))
+        for n, tmp_df in best_configs.iterrows():
+            config = json.loads(tmp_df.to_json())
+            config.pop('accuracy')
+            config.pop('max_length')
+            scorer = alm.RelationScorer(model=config.pop('model'), max_length=_len)
+            scorer.analogy_test(
+                no_inference=no_inference,
+                test=True,
+                export_prefix=export_prefix,
+                batch_size=_batch,
+                val_accuracy=val_accuracy,
+                **config
+            )
+            scorer.release_cache()
+    alm.export_report(export_prefix=export_prefix, test=True)
 
-
-logging.info('#######################################################')
-logging.info('# Get prediction on each configuration (on valid set) #')
-logging.info('#######################################################')
-methods += ['ppl_add_masked', 'ppl_marginal_bias', 'ppl_hypothesis_bias']
-positive_permutation_aggregation = [
-    'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7'
-]
-pmi_feldman_aggregation = [
-    'max', 'mean', 'min', 'index_0', 'index_1', 'index_2', 'index_3', 'index_4', 'index_5', 'index_6', 'index_7',
-    'index_8', 'index_9', 'index_10', 'index_11'
-]
-ppl_based_pmi_aggregation = ['max', 'mean', 'min', 'index_0', 'index_1']
-export_prefix = 'experiment.scoring_comparison'
-no_inference = False
-for _model, _max_length, _batch in models:
-    for scoring_method in methods:
-        if 'gpt' in _model and scoring_method in methods_mlm:
-            continue
-        scorer = alm.RelationScorer(model=_model, max_length=_max_length)
-        for _data in data:
-            for _temp in all_templates:
-                shared = dict(
-                    scoring_method=scoring_method,
-                    data=_data,
-                    template_type=_temp,
-                    batch_size=_batch,
-                    export_prefix=export_prefix,
-                    no_inference=no_inference,
-                    positive_permutation_aggregation=positive_permutation_aggregation,
-                    ppl_based_pmi_aggregation=ppl_based_pmi_aggregation
-                )
-                if scoring_method == 'pmi_feldman':
-                    for i in pmi_feldman_aggregation:
-                        scorer.analogy_test(pmi_feldman_aggregation=i, **shared)
-                        scorer.release_cache()
-                else:
-                    scorer.analogy_test(**shared)
-                scorer.release_cache()
-alm.export_report(export_prefix=export_prefix)
-
-
-logging.info('#######################################################################')
-logging.info('# get test accuracy on each combination of model and scoring function #')
-logging.info('#######################################################################')
-no_inference = False
-export_prefix = 'experiment.scoring_comparison'
-df = alm.get_report(export_prefix=export_prefix)
-for i, m, s in product(data, models, methods):
-
-    _model, _len, _batch = m
-    if 'gpt' in _model and s in methods_mlm:
-        continue
-    tmp_df = df[df.data == i]
-    tmp_df = tmp_df[tmp_df.model == _model]
-    tmp_df = tmp_df[tmp_df.scoring_method == s]
-    val_accuracy = tmp_df.sort_values(by='accuracy', ascending=False).head(1)['accuracy'].values[0]
-    logging.info("RUN TEST:\n - data: {} \n - lm: {} \n - score: {} \n - validation accuracy: {} ".format(
-        i, _model, s, val_accuracy))
-    best_configs = tmp_df[tmp_df['accuracy'] == val_accuracy]
-    logging.info("find {} configs with same accuracy".format(len(best_configs)))
-    for n, tmp_df in best_configs.iterrows():
-        config = json.loads(tmp_df.to_json())
-        config.pop('accuracy')
-        config.pop('max_length')
-        scorer = alm.RelationScorer(model=config.pop('model'), max_length=_len)
+if not SKIP_DEFAULT:
+    logging.info('############################################################')
+    logging.info('# get test accuracy on each default configuration of model #')
+    logging.info('############################################################')
+    no_inference = False
+    shared = {'template_type': 'is-to-as', 'scoring_method': 'ppl_based_pmi'}
+    export_prefix = 'experiment.scoring_comparison.default'
+    for i, m in product(data, models):
+        _model, _len, _batch = m
+        scorer = alm.RelationScorer(model=_model, max_length=_len)
         scorer.analogy_test(
-            no_inference=no_inference,
-            test=True,
-            export_prefix=export_prefix,
-            batch_size=_batch,
-            val_accuracy=val_accuracy,
-            **config
-        )
+            no_inference=no_inference, batch_size=_batch, data=i, export_prefix=export_prefix, test=False, **shared)
+        scorer.analogy_test(
+            no_inference=no_inference, batch_size=_batch, data=i, export_prefix=export_prefix, test=True, **shared)
         scorer.release_cache()
-alm.export_report(export_prefix=export_prefix, test=True)
-
-
-logging.info('############################################################')
-logging.info('# get test accuracy on each default configuration of model #')
-logging.info('############################################################')
-no_inference = False
-shared = {'template_type': 'is-to-as', 'scoring_method': 'ppl_based_pmi'}
-export_prefix = 'experiment.scoring_comparison.default'
-for i, m in product(data, models):
-    _model, _len, _batch = m
-    scorer = alm.RelationScorer(model=_model, max_length=_len)
-    scorer.analogy_test(
-        no_inference=no_inference, batch_size=_batch, data=i, export_prefix=export_prefix, test=False, **shared)
-    scorer.analogy_test(
-        no_inference=no_inference, batch_size=_batch, data=i, export_prefix=export_prefix, test=True, **shared)
-    scorer.release_cache()
-alm.export_report(export_prefix=export_prefix, test=False)
-alm.export_report(export_prefix=export_prefix, test=True)
+    alm.export_report(export_prefix=export_prefix, test=False)
+    alm.export_report(export_prefix=export_prefix, test=True)
 
---
+++ experiments/summary_2021_acl.py
@@ -0,0 +1,115 @@
+# Aggregate result for the paper
+import pandas as pd
+
+print('\n#################################')
+print('## SCORING FUNCTION COMPARISON ##')
+print('#################################')
+data = ['sat', 'u2', 'u4', 'google', 'bats']
+models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 256), ('bert-large-cased', 32, 1024)]
+scoring_method = ['ppl_hypothesis_bias', 'ppl_marginal_bias', 'ppl_based_pmi']
+methods = ['pmi_feldman', 'embedding_similarity', 'ppl']
+df = pd.read_csv('experiments_results/summary/experiment.scoring_comparison.test.csv')
+for d in data:
+    df_tmp = df[df.data == d]
+    print('DATASET: {}'.format(d))
+    for model, _, _ in models:
+        print('- {}'.format(model))
+        df_tmp_tmp = df_tmp[df_tmp.model == model]
+        for method in methods:
+            df_tmp_tmp_tmp = df_tmp_tmp[df_tmp_tmp.scoring_method == method]
+            if len(set(list(df_tmp_tmp_tmp['accuracy_validation']))) == 1:
+                acc_val = round(list(df_tmp_tmp_tmp['accuracy_validation'])[0] * 100, 1)
+                acc = [round(i * 100, 1) for i in list(df_tmp_tmp_tmp['accuracy'])]
+                acc = acc[int(len(acc)/2)]
+                print('\t - {:<20}: {} (validation {})'.format(method, acc, acc_val))
+
+print('\n###########################')
+print('## DEFAULT PPL BASED PMI ##')
+print('###########################')
+df_test = pd.read_csv('experiments_results/summary/experiment.scoring_comparison.default.test.csv')
+df_val = pd.read_csv('experiments_results/summary/experiment.scoring_comparison.default.valid.csv')
+for d in data:
+    df_test_tmp = df_test[df_test.data == d]
+    df_val_tmp = df_val[df_val.data == d]
+    print('DATASET: {}'.format(d))
+    for model, _, _ in models:
+        df_val_tmp_tmp = df_val_tmp[df_val_tmp.model == model]
+        df_test_tmp_tmp = df_test_tmp[df_test_tmp.model == model]
+
+        if len(set(list(df_val_tmp_tmp['accuracy']))) == 1:
+            acc_val = round(list(df_val_tmp_tmp['accuracy'])[0] * 100, 1)
+            acc_test = round(list(df_test_tmp_tmp['accuracy'])[0] * 100, 1)
+            if d == 'sat':
+                full_acc = round((37 * acc_val + 337 * acc_test)/374, 1)
+                print('- {:<20}: {} (validation {}, full {})'.format(model, acc_test, acc_val, full_acc))
+            else:
+                print('- {:<20}: {} (validation {})'.format(model, acc_test, acc_val))
+
+print('\n#############################')
+print('## WORD EMBEDDING BASELINE ##')
+print('#############################')
+df_test = pd.read_csv('./experiments_results/summary/experiment.word_embedding.test.csv')
+df_val = pd.read_csv('./experiments_results/summary/experiment.word_embedding.valid.csv')
+df_test_pmi = pd.read_csv('./experiments_results/summary/experiment.pmi.test.csv')
+df_valid_pmi = pd.read_csv('./experiments_results/summary/experiment.pmi.valid.csv')
+models = ['w2v', 'fasttext', 'glove']
+for d in data:
+    df_test_tmp = df_test[df_test.data == d]
+    df_val_tmp = df_val[df_val.data == d]
+    pmi_val = round(list(df_valid_pmi[df_valid_pmi.data == d]['pmi'])[0] * 100, 1)
+    pmi_test = round(list(df_test_pmi[df_test_pmi.data == d]['pmi'])[0] * 100, 1)
+    print('DATASET: {}'.format(d))
+    if d == 'sat':
+        full_acc = round((37 * pmi_val + 337 * pmi_test) / 374, 1)
+        print('- {:<20}: {} (validation {}, full {})'.format('pmi', pmi_test, pmi_val, full_acc))
+    else:
+        print('- {:<20}: {} (validation {})'.format('pmi', pmi_test, pmi_val))
+    for model in models:
+        acc_val = round(float(df_val_tmp[model]) * 100, 1)
+        acc_test = round(float(df_test_tmp[model]) * 100, 1)
+        if d == 'sat':
+            full_acc = round((37 * acc_val + 337 * acc_test) / 374, 1)
+            print('- {:<20}: {} (validation {}, full {})'.format(model, acc_test, acc_val, full_acc))
+        else:
+            print('- {:<20}: {} (validation {})'.format(model, acc_test, acc_val))
+
+print('\n############################')
+print('## TUNED AP PPL-PMI SCORE ##')
+print('############################')
+models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 256), ('bert-large-cased', 32, 1024)]
+for d in data:
+    df = pd.read_csv('./experiments_results/summary/experiment.ppl_variants.full.{}.csv'.format(d))
+    df = df[df.scoring_method == 'ppl_based_pmi']
+    print('DATASET: {}'.format(d))
+    for model, _, _ in models:
+        df_tmp = df[df.model == model]
+        df_tmp = df_tmp.sort_values(by=['accuracy_validation'], ascending=False)
+        acc_val = list(df_tmp.head(1)['accuracy_validation'])[0]
+        acc = df_tmp[df_tmp.accuracy_validation == acc_val].sort_values(by=['accuracy_test'])
+        acc_test = list(acc['accuracy_test'])
+
+        acc_test = acc_test[int(len(acc_test)/2)]
+
+        acc_val = round(acc_val * 100, 1)
+        acc_test = round(acc_test * 100, 1)
+        acc_full = list(acc['accuracy'])[0]
+        full_acc = round(acc_full * 100, 1)
+        print('- {:<20}: {} (validation {}, full {})'.format(model, acc_test, acc_val, full_acc))
+
+print('\n################################################')
+print('## TUNED AP PPL-PMI SCORE (TUNED ON FULL SET) ##')
+print('################################################')
+models = [('roberta-large', 32, 512), ('gpt2-xl', 32, 256), ('bert-large-cased', 32, 1024)]
+for d in data:
+    df = pd.read_csv('./experiments_results/summary/experiment.ppl_variants.full.{}.csv'.format(d))
+    df = df[df.scoring_method == 'ppl_based_pmi']
+    print('DATASET: {}'.format(d))
+    for model, _, _ in models:
+        df_tmp = df[df.model == model]
+        df_tmp = df_tmp.sort_values(by=['accuracy'], ascending=False)
+        acc = list(df_tmp.head(1)['accuracy'])[0]
+
+        acc = round(acc * 100, 1)
+        print('- {:<20}: {}'.format(model, acc))
+
+

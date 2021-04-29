6065b9eb1be6882e37f9d5021b9a457872b8c06d
asahi417 <asahi1992ushio@gmail.com>
2021-04-30 00:11:17 +0100

    fix

--- tmp.py
+++
@@ -1,9 +0,0 @@
-import json
-from glob import glob
-
-for i in glob('experiments_results/logit/*/*/ppl_based_pmi/*/config.json'):
-    print(i)
-    config = json.load(open(i))
-    config['scoring_method'] = 'ppl_based_pmi'
-    with open(i, 'w') as f:
-        json.dump(config, f)
